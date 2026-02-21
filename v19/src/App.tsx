bb97f4f59e9aa5be3198d6340c442d7221b6380a
Chintana Meegamarachchi <chintana@appsolve.com.au>
2026-02-21 09:36:02 +0000

    CI

--- v19/src/App.tsx
+++ v19/src/App.tsx
@@ -1,9 +1,41 @@
-import { Button } from "@/components/ui/button"
- 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./features/home";
+import { ErrorPage, Site404Page } from "./features/core";
+import { ContactsPage } from "./features/contacts";
+import { SettingsPage } from "./features/settings";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contacts",
+    element: <ContactsPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/contacts/all",
+    element: <ContactsPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/settings",
+    element: <SettingsPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "*",
+    element: <Site404Page />,
+  },
+]);
+
+
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   )
 }
