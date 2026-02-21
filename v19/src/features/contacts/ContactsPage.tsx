3442bb0642b8807be2febd215020ce93c2648695
Chintana Meegamarachchi <chintana@appsolve.com.au>
2026-02-21 12:33:41 +0000

    Adds contact form

--- v19/src/features/contacts/ContactsPage.tsx
+++ v19/src/features/contacts/ContactsPage.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import type { BreadCrumbItem } from "@/components/SideNav/model";
 
-const ContactsPage: React.FC = () => {
+import Layout from "./../Layout";
+import ContactsDataGrid from "./ContactList";
+import { ContactContextProvider } from "./ContectProvider";
+
+const breadCrumbs: BreadCrumbItem[] = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "Contacts",
+    url: "/contacts",
+  },
+];
+
+const ContactsPage = () => {
   return (
-    <div>Contacts</div>
-  )
-}
+    <Layout titleToActivate="Contacts" breadcrumbs={breadCrumbs}>
+      <Layout.Title>Contacts</Layout.Title>
+      <ContactContextProvider>
+        <ContactsDataGrid />
+      </ContactContextProvider>
+    </Layout>
+  );
+};
 
-export default ContactsPage
+export default ContactsPage;
